837d6e25f88833c43db2262f71af51482d42735c
Hankus Pankus <henryjacobscode@gmail.com>
2022-10-20 11:59:43 -0400

    Starting Javascript

---
+++ Odin-Project/Foundations/javascript/first-go/secondscript.js
@@ -0,0 +1,29 @@
+/* Infinity and NAN */
+
+console.log(1/0);
+console.log(-1/0); 
+console.log( "Not a number" / 2);
+console.log(NaN + 1); 
+console.log(3 * NaN); 
+console.log("Not a number" / 2 - 1);
+
+/* Strings */
+
+let str = "Hello" ; 
+let str2 = 'Single Quote are ok too' ;
+let phrase = `can embed another ${str}`; 
+
+let name2 = "John"; 
+console.log(`Hello ${name2}`); 
+console.log(`The result is ${1+2}`); 
+
+
+/* Boolean (logical type) */ 
+let nameFieldChecked = true ; 
+let ageFieldChecked = false ; 
+
+let isGreater = 4 > 1 ;
+console.log(isGreater);
+
+
+/* null and undefined types */
