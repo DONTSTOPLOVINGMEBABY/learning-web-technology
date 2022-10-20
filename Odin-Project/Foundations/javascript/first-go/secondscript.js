e4b0bdf0395949590f0dc9cf8baa5c7249c876ec
Hankus Pankus <henryjacobscode@gmail.com>
2022-10-20 12:13:22 -0400

    more javascript

--- Odin-Project/Foundations/javascript/first-go/secondscript.js
+++ Odin-Project/Foundations/javascript/first-go/secondscript.js
@@ -9,6 +9,8 @@ console.log("Not a number" / 2 - 1);
 
 /* Strings */
 
+console.log("Srings\n\n\n\n")
+
 let str = "Hello" ; 
 let str2 = 'Single Quote are ok too' ;
 let phrase = `can embed another ${str}`; 
@@ -16,6 +18,17 @@ let phrase = `can embed another ${str}`;
 let name2 = "John"; 
 console.log(`Hello ${name2}`); 
 console.log(`The result is ${1+2}`); 
+
+const string1 = "The revolution will not be televised."; 
+console.log(string1);
+
+const sglDbl = 'Would you eat a "fish supper"?';
+const dblSgl = "I'm feeling blue." ;
+console.log(sglDbl);
+console.log(dblSgl);
+
+const bigMouth = 'I\'ve got the no right to take my place...';
+console.log(bigMouth);
 
 
 /* Boolean (logical type) */ 
@@ -27,3 +40,24 @@ console.log(isGreater);
 
 
 /* null and undefined types */
+
+//undefined
+let x ; 
+console.log(x);
+//null
+x = null ; 
+console.log(x);
+
+
+/* Typeof operator */ 
+
+console.log("Type of Operators\n\n\n\n")
+console.log(typeof undefined);
+console.log(typeof 0);
+console.log(typeof 10n);
+console.log(typeof true);
+console.log(typeof "foo");
+console.log(typeof Symbol("id"));
+console.log(typeof Math);
+console.log(typeof null);
+console.log(typeof alert); 
