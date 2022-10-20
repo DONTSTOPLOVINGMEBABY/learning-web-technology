a73afc43475f5a1a66c75531c611caa96512487c
Hankus Pankus <henryjacobscode@gmail.com>
2022-10-20 14:13:01 -0400

    Hashing through Javascript fundamentals

--- Odin-Project/Foundations/javascript/first-go/secondscript.js
+++ Odin-Project/Foundations/javascript/first-go/secondscript.js
@@ -84,30 +84,101 @@ let string7 = "       Hello!         " ;
 console.log("Before trim: ", string7, "*");
 console.log("After Trim: ", string7.trim(), "*");
 console.log("After Trim Start: ", string7.trimStart(), "*");
-
-
+//padding a string
+let string8 = "5";
+let padded = string8.padStart(5, "0"); 
+console.log(padded);
+string8 = "532" ; 
+padded = string8.padStart(5, "0"); //Replaces up to 5 spaces, never overwrites
+console.log(padded);
+string8 = "5332244" ; 
+padded = string8.padStart(5, "0"); // Has no effect
+console.log(padded); 
+string8 = "532"; 
+padded = string8.padEnd(5, "0"); 
+console.log(padded);
+//Get specific character in a string (charAt method) and character code
+let string9 = "HELLO WORLD!"; 
+let character = string9.charAt(0); // can also use string[] selector
+console.log(character, string9[0], string9.charCodeAt(0));
+//String Split 
+let string10 = "This is a big string that can be split by space, and comma."; 
+console.log("The string being split: ",string10);
+let splitstring = string10.split(" "); 
+let splitstring2 = string10.split(",");
+console.log("Split by Space: ", splitstring) ; // turns it into a list
+console.log("Split by comma: ", splitstring2); 
 
 
 /****************************************************************************/
 
 
 
-/* Boolean (logical type) */ 
+/* Boolean (logical type) ***************************************************/ 
+
+
 let nameFieldChecked = true ; 
 let ageFieldChecked = false ; 
 
 let isGreater = 4 > 1 ;
 console.log(isGreater);  
 
-
-/* null and undefined types */
-
-//undefined
+//Comparisons
+console.log("Comparisons");
+console.log(2>1) ; //true
+console.log(2 == 1); //false 
+console.log(2!=1) ; 
+let result = 3 > 1 ; 
+console.log(result);
+//String Comparisons
+console.log("Z > A", 'Z' > 'A');
+console.log("Glow > Glee", 'Glow' > 'Glee'); 
+console.log("Bee > Be", 'Bee' > 'Be') ; // It compares character by character until it finds a character that's either lower or greater
+console.log("Bee > Beadf", 'Bee' < 'Beadf'); // false
+//Comparisons of different types
+console.log('2' > 1); //true, string 2 becomes a number
+console.log('01'==1); // true, string '01' becomes a number 1. For boolean values, true becomes 1 and false becomes 0
+//Strict Equality
+console.log("0 == false", 0 == false); //true
+console.log("''==false", ''==false); // true ... '' is converted to a 0 
+console.log("0 === false", 0 === false) //false, they are not exactly the same
+console.log("null===undefined", null===undefined); //false
+console.log("null == undefined"); //true
+//undefined ... when converted to a number, undefined becomes a NaN
 let x ; 
 console.log(x);
-//null
+//null... When converted to a number null becomes 0. It's converted to a number with comparison operators and not equality?
 x = null ; 
 console.log(x);
+
+/****************************************************************************/
+
+
+/*******************Conditionals*********************************************/
+
+let truth = true ; 
+let nottruth = false; 
+let num = 7 ; 
+
+if (truth) {
+    console.log("Truth executed"); 
+}
+
+if (nottruth){
+    console.log("Not truth executed") ; 
+} else {
+    console.log("The else statement executed");
+}
+
+if (!truth) {
+    console.log("Logical not truth executed"); 
+}   else if (num == 7) {
+    console.log("Nottruth executed") ; 
+} else {
+    console.log("Reached the else statement"); 
+}
+
+
 
 
 /* Typeof operator */ 
