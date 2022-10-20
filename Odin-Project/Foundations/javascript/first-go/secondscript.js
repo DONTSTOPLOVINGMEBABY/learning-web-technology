3d0134063b11ae73b9956af51db9a01876f89cd5
Hankus Pankus <henryjacobscode@gmail.com>
2022-10-20 13:07:33 -0400

    Working on strings

--- Odin-Project/Foundations/javascript/first-go/secondscript.js
+++ Odin-Project/Foundations/javascript/first-go/secondscript.js
@@ -7,7 +7,8 @@ console.log(NaN + 1);
 console.log(3 * NaN); 
 console.log("Not a number" / 2 - 1);
 
-/* Strings */
+
+/* Strings *******************************************************************/
 
 console.log("Srings\n\n\n\n")
 
@@ -50,9 +51,45 @@ const output = `I like the song ${song}. I gave it a score of ${
 }%.`; 
 console.log(output); 
 
+
+//String Methods
+//Slice/Substring
 const bigString = "Henry is at the park smoking weed"; 
 console.log(bigString, bigString.length);
 console.log(bigString.slice(bigString.length-4,bigString.length))
+console.log(bigString.slice(-12,-5)) // prints starting 12 to the left up to but not including 5 to the left
+console.log(bigString.substring(5)); // prints rest string starting from 5
+console.log(bigString.substring(-21, 19)); // treats negative numbers as zero (prints elements 0-19)
+//Replace
+let text = "Please visit Microsoft!"; 
+let newText = text.replace("Microsoft", "W3Schools"); 
+console.log(text, newText);
+let newestText = text.replace("MICROSOFT", "This Shouldn't Work"); //Will just assign the variable the original string
+console.log(newestText)
+let workingSolution = text.replace(/MICROSOFT/i, "W3Schools"); //Case insensitive
+console.log(workingSolution)
+//toUpper and toLower
+let stringmixed = 'henRy Is codIng pEacefully'; 
+console.log(stringmixed.toUpperCase(), stringmixed.toLowerCase());
+//concat
+let string4 = "Henry is ";
+let string5 = "Coding Peacefully"; 
+let stringmaster = "" ; stringmaster += string4 + string5 ; 
+console.log(stringmaster);
+let string6 = "In the Park"
+let stringmaster2 = stringmaster.concat(" ", string6); // can also just do stringmaster.concact(string6);
+console.log(stringmaster2); 
+//trim 
+let string7 = "       Hello!         " ; 
+console.log("Before trim: ", string7, "*");
+console.log("After Trim: ", string7.trim(), "*");
+console.log("After Trim Start: ", string7.trimStart(), "*");
+
+
+
+
+/****************************************************************************/
+
 
 
 /* Boolean (logical type) */ 
@@ -60,7 +97,7 @@ let nameFieldChecked = true ;
 let ageFieldChecked = false ; 
 
 let isGreater = 4 > 1 ;
-console.log(isGreater);
+console.log(isGreater);  
 
 
 /* null and undefined types */
