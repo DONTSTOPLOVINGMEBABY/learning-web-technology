8993caf1feb3902940530229d789968a22cf7f4c
Hankus Pankus <henryjacobscode@gmail.com>
2022-10-22 17:41:25 -0400

    FizzBuzz

---
+++ Odin-Project/Foundations/javascript/first-go/fizzbuzz.js
@@ -0,0 +1,24 @@
+console.log("Hello, we are solving Fizz Buzz today.");
+
+let answer = parseInt
+
+
+
+
+
+// Expected Output
+// 3 = Fizz
+// 5 = Buzz
+// ...
+// 15 = Fizz Buzz
+// 18 = Fizz
+// ...
+// 25 = Buzz 
+// 30 = Fizz Buzz
+
+
+//Psuedo
+// if number mod 3 == 0 and number mod 3 == 5 then Fizz Buzz
+// elif number mod 3 == 0 then Fizz
+// elif number mod 5 == 0 then Buzz 
+// else print number 
