4236361af407e927f472bec0a13d4d1a7709b72d
Hankus Pankus <henryjacobscode@gmail.com>
2022-10-25 16:16:30 -0400

    End of day commit

--- Odin-Project/Foundations/javascript/first-go/arrays-loops.js
+++ Odin-Project/Foundations/javascript/first-go/arrays-loops.js
@@ -26,8 +26,50 @@ cars.forEach(hello)
 function hello (element) {
     console.log(`Hello ${element}`);
 }
-
+//Can you spot the difference between while and do while?
 let whilectr = 0 ; 
-while (whilectr < 10){
+while (whilectr < 4){
     console.log(2 ** whilectr);
+    whilectr++;
 }
+
+whilectr= 0 ;
+
+do {
+    console.log(2 ** whilectr);
+} while (whilectr++ < 4)
+
+//You can skip first entry in for loop:
+let forctr = 4 ; 
+for (; forctr < 5 ; forctr++){forctr = forctr ** 3;}
+console.log("Forctr ", forctr) ;
+
+//break <labelName>
+
+let variable = 0 ; 
+outer: for (let i = 0 ; i < 4 ; i++){
+    variable++
+    console.log("Variable: ", variable)
+    console.log("i: ", i);
+    for (let j = 0 ; j < i ; j++){
+        if (variable == 2){
+            break outer;
+        }
+    }
+
+}
+
+//an interesting one...
+//remember 0 == false and 1 == true
+
+let ctr = 3 ; 
+while (ctr) {
+    console.log( --ctr );
+}
+
+function functionarguments () {
+    let args = Array.from(arguments);
+    console.log(args);
+}
+
+functionarguments(1, "Henry", ["a", "list"]);
