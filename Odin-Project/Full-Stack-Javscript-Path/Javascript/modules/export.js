7cfcba4d67b975c7757720709240d7266f5bcc75
Henry Jacobs <henryjacobscode@gmail.com>
2022-12-07 11:53:32 -0500

    Update .gitignore and try module examples out

---
+++ Odin-Project/Full-Stack-Javscript-Path/Javascript/modules/export.js
@@ -0,0 +1,16 @@
+// const Factory_Function = (arg1, arg2) => {
+    
+//     const pretty_print = () => {console.log(`${arg1}, ${arg2}`)} ; 
+//     return { arg1, arg2, pretty_print }
+// }
+
+// const capitalize_all_args = (factory_func) => {
+//     return `${factory_func.arg1.toUpperCase()} and ${factory_func.arg2.toUpperCase()}`; 
+// }
+
+// export default Factory_Function ; 
+// export { capitalize_all_args }; 
+
+const functionOne = () => console.log('FUNCTION ONE!');
+
+export { functionOne };
---
+++ Odin-Project/Full-Stack-Javscript-Path/Javascript/modules/import.js
@@ -0,0 +1,11 @@
+// import Factory_Function, { capitalize_all_args } from "./export";
+
+// const here_we_are = Factory_Function("Hello", "There"); 
+// here_we_are.pretty_print(); 
+
+// console.log(capitalize_all_args(here_we_are))
+
+// another JS file
+import { functionOne } from './export.js';
+
+functionOne(); // this should work as expected!
