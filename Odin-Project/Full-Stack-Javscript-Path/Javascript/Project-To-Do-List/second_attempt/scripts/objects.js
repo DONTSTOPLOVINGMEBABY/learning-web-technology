b6bc3558b03aea9a964800c1de12f91b9a3cabb0
Hankus Pankus <henryjacobscode@gmail.com>
2022-12-17 15:12:22 -0500

    Finish all objects (I think). Add containers for projects, notes, todos, and lists

--- Odin-Project/Full-Stack-Javscript-Path/Javascript/Project-To-Do-List/second_attempt/scripts/objects.js
+++ Odin-Project/Full-Stack-Javscript-Path/Javascript/Project-To-Do-List/second_attempt/scripts/objects.js
@@ -33,15 +33,14 @@ class To_Dos {
     }
 }
 
-
 function Aggregate_Todos () {
     this.bucket_todos = [] ; 
 
     this.delete_todo = function (name) {
         for (let i = 0 ; i < this.bucket_todos.length ; i++){
-            if (this.bucket_todos[i].title == name){
-                this.bucket_todos.slice(i, i) ;
-                break ; 
+            if (this.bucket_todos[i].title == name.title){
+                this.bucket_todos.splice(i, i + 1) ;
+                break
             }
         }
     }
@@ -51,6 +50,64 @@ function Aggregate_Todos () {
     }
 }
 
+function Aggregate_Notes () {
+    this.bucket_notes = [] ; 
 
+    this.delete_note = function (name) {
+        for (let i = 0 ; i < this.bucket_notes.length ; i++){
+            if (this.bucket_notes[i].title == name.title){
+                this.bucket_notes.splice(i, i + 1) ;
+                break
+            }
+        }
+    }
 
-export { Note, Project, List, To_Dos, Aggregate_Todos } ; 
+    this.add_note = function (note) {
+        this.bucket_notes.push(note) ; 
+    }
+}
+
+function Aggregate_Lists () {
+    this.bucket_lists = [] ; 
+
+    this.delete_list = function (name) {
+        for (let i = 0 ; i < this.bucket_lists.length ; i++){
+            if (this.bucket_lists[i].title == name.title){
+                this.bucket_lists.splice(i, i + 1) ;
+                break
+            }
+        }
+    }
+    
+    this.add_list = function (list) {
+        this.bucket_lists.push(list) ; 
+    }
+}
+
+function Aggregate_Projects () {
+    this.bucket_projects = [] ; 
+
+    this.delete_project = function (name) {
+        for (let i = 0 ; i < this.bucket_projects.length ; i++){
+            if (this.bucket_projects[i].title == name.title){
+                this.bucket_projects.splice(i, i + 1) ;
+                break
+            }
+        }
+    }
+
+    this.add_project = function (project) {
+        this.bucket_projects.push(project) ; 
+    }
+}
+
+export { 
+    Note, 
+    Project, 
+    List, 
+    To_Dos, 
+    Aggregate_Todos, 
+    Aggregate_Notes, 
+    Aggregate_Lists, 
+    Aggregate_Projects, 
+} ; 
