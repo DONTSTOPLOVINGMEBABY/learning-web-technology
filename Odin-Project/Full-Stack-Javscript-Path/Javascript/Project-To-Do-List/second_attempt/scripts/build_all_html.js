e29a1f109850652a4bb5e2f4851b13e4c5e008c0
Hankus Pankus <henryjacobscode@gmail.com>
2022-12-18 14:59:45 -0500

    Redo make_toDos so that it only relies on data from one object. Add functionality to not allow for duplicates

--- Odin-Project/Full-Stack-Javscript-Path/Javascript/Project-To-Do-List/second_attempt/scripts/build_all_html.js
+++ Odin-Project/Full-Stack-Javscript-Path/Javascript/Project-To-Do-List/second_attempt/scripts/build_all_html.js
@@ -4,14 +4,19 @@ function elementFromHtml(html) {
     return template.content; 
 }
 
-const todos_already_loaded = {} ; 
+function make_projects(list_of_projects, append_element) {} ; 
+function make_lists(list_of_lists, append_element) {} ; 
+function make_notes(list_of_notes, append_element) {} ; 
 
+function make_toDos(all_todos_object, append_element) {
 
-function make_toDos(list_of_todos, append_element) {
+    let list_of_todos = all_todos_object.bucket_objects ; 
+    let list_of_added_items = all_todos_object.array_of_loaded_objects ; 
+    let array_of_objects = all_todos_object.array_of_objects ; 
 
     function make_one_todo (title, due_date, priority, project){
         const singular_todo = `
-        <div class="singular-todo">
+        <div class="singular-todo" id="the-${project}">
             <div class="priority-color ${priority}"></div>
             <div class="to-do-title">${title}</div>
             <div class="part-of-project">Project: ${project}</div>
@@ -25,18 +30,54 @@ function make_toDos(list_of_todos, append_element) {
         return elementFromHtml(singular_todo) 
     }
 
-    for (let i = 0 ; i < list_of_todos.length ; i++){
-
-        if (!todos_already_loaded[list_of_todos[i].project]) {
-            let todo = make_one_todo(list_of_todos[i].title, list_of_todos[i].due_date, list_of_todos[i].priority, list_of_todos[i].project) ; 
-            append_element.append(todo)
-            todos_already_loaded[list_of_todos[i].project] = list_of_todos[i].title ; 
+    for (let todo in list_of_todos){
+        let found = false ; 
+        for (let added_object in list_of_added_items){
+            if (added_object != todo) { continue }
+            else if (added_object == todo) { 
+                found = true ; 
+                break ;
+            }
+        }
+        if (found) {continue} 
+        else {
+            let string = todo + "-" + list_of_todos[todo]
+            let todo_from_list = array_of_objects[string]
+            let append_todo = make_one_todo(todo_from_list.title, todo_from_list.due_date, todo_from_list.priority, todo_from_list.project) ; 
+            append_element.append(append_todo) ; 
+            all_todos_object.load_object(todo_from_list) ; 
         }
     }
-
-    console.log(todos_already_loaded); 
+    
+    make_to_do_buttons_function(all_todos_object, array_of_objects) ; 
 
 }
+
+function make_to_do_buttons_function (all_todos_object, array_of_objects) {
+    const all_to_do_pencils = document.querySelectorAll('#big-pencil') ; 
+    const all_to_do_exit_buttons = document.querySelectorAll("#big-delete") ; 
+
+    function delete_button (element) {
+        let this_singular_todo = element.target.parentElement.parentElement ; 
+        let project_name = this_singular_todo.id.split("-")[1] ; 
+        let grab_todo = project_name + "-" + all_todos_object.bucket_objects[project_name] ; 
+        grab_todo = all_todos_object.array_of_objects[grab_todo]
+        all_todos_object.delete_object(grab_todo) ; 
+        this_singular_todo.remove() ; 
+    }
+
+    all_to_do_pencils.forEach( (element) => {
+        element.addEventListener('click', () => { console.log("Working for me") }) 
+    })
+
+    all_to_do_exit_buttons.forEach( (element) => {
+        element.addEventListener('click', (element) => { delete_button(element) }) 
+    })
+
+}
+
+
+
 
 
 
