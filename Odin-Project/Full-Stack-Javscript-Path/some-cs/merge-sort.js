1e1b210b686c838896f9a179b55fd01c1bbdbf6e
Henry Jacobs <henryjacobscode@gmail.com>
2023-01-12 16:17:04 -0500

    Upload Merge Sort

---
+++ Odin-Project/Full-Stack-Javscript-Path/some-cs/merge-sort.js
@@ -0,0 +1,28 @@
+/* 
+    Psuedo Code from David Malan
+
+    on input of n elements:
+        if n < 2
+            return 
+        else
+            sort left half of elements
+            sort right half of elements
+            merge sorted values
+
+    Psuedo Code from Abdul Bari
+
+
+        
+
+*/ 
+
+
+function merge_sort_recursive(list) {
+    if (list.length < 2){
+        return
+    }
+    else{
+    }
+}
+
+console.log(merge_sort_recursive([2, 1, 6, 3, 5, 4, 8 ,7])) ; 
