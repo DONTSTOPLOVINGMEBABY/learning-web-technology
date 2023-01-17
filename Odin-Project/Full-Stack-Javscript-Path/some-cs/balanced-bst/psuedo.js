18b3fab21f657d8f0c00487124aefed350bf46cd
Henry Jacobs <henryjacobscode@gmail.com>
2023-01-17 16:06:19 -0500

    init even binary trees

---
+++ Odin-Project/Full-Stack-Javscript-Path/some-cs/balanced-bst/psuedo.js
@@ -0,0 +1,8 @@
+/* 
+1. Set The middle element of the array as root.
+2. Recursively do the same for the left half and right half.
+3. Get the middle of the left half and make it the left child of the root created in step 1.
+4. Get the middle of the right half and make it the right child of the root created in step 1.
+5. Print the preorder of the tree.
+*/ 
+
