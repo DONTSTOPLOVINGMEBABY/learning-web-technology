c0eb6604982f4b785d0789a866317e46d880085e
Henry Jacobs <henryjacobscode@gmail.com>
2023-02-13 16:00:23 -0500

    Make simple form that writes to firebase

---
+++ Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/learn-firebase/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from './comps/homs';
+
+function App() {
+  return (
+    <div className="App">
+      Save Time
+      <Home />
+    </div>
+  );
+}
+
+export default App;
