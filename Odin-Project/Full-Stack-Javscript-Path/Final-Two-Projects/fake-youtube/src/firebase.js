d654216cd2a4aa56aa9f9245119c6692c13294a6
Hankus Pankus <henryjacobscode@gmail.com>
2023-02-21 21:41:43 -0500

    Rename final js proj to fake-youtube proj

---
+++ Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKAdCQp4uPK1dgYvRAdOdVMxvPUvBHJzc",
+  authDomain: "fake-32e47.firebaseapp.com",
+  projectId: "fake-32e47",
+  storageBucket: "fake-32e47.appspot.com",
+  messagingSenderId: "261979478486",
+  appId: "1:261979478486:web:a95042d0e25c7002212c77"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
