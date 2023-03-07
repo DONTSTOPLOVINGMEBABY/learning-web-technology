05ed93d4592ce6fa33d4ed466063eb2b0b749bce
Henry Jacobs <henryjacobscode@gmail.com>
2023-03-07 17:04:23 -0500

    Add new routes and remove react.strictmode

--- Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/RouteSwitch.js
+++ Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/RouteSwitch.js
@@ -4,7 +4,7 @@ import HomePage from "./components/video-components/HomePage";
 import SideBar from "./components/sidebar/signed-in-sidebar";
 import MiniSideBar from "./components/sidebar/mini-sidebar";
 import SignedOutSideBar from "./components/sidebar/signed-out-sidebar";
-import Single from "./pages/single";
+import PlayVideo from "./components/video-components/VideoPlayer";
 import { useEffect, useMemo, useState } from "react";
 import { userContext } from "./components/utils/contexts";
 import './styles/App.css';
@@ -36,8 +36,16 @@ const RouteSwitch = () => {
                     <> <SignedOutSideBar /> <MiniSideBar /> </>
                     }
                     <Routes>
-                        <Route exact path="/" element={<HomePage/>}/>
-                        <Route exact path="/rock" element={<Single/>}/>
+                        <Route exact path="/" element={<HomePage 
+                        main_container="video-container"
+                        video_class="video"
+                        video_information="video-information-main"
+                        profile_picture="profile-picture-main"
+                        profile_photo="profile-photo-homepage-player" 
+                        channel_video_information="channel-video-information-homepage"
+                        video_title="video-title-homepage"
+                        />}/>
+                        <Route exact path="/video-player/:id" element={<PlayVideo/>}/>
                     </Routes>
                 </div>
             </userContext.Provider>
