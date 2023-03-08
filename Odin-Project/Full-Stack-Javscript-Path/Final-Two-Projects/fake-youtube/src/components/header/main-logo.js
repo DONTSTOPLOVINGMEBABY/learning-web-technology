1a8141766971e0746e9da867154a9b572a56a593
Henry Jacobs <henryjacobscode@gmail.com>
2023-03-08 10:45:51 -0500

    Finish adding menu-size option functionality

--- Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/components/header/main-logo.js
+++ Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/components/header/main-logo.js
@@ -1,22 +1,38 @@
 import youtube_icon from "../assets/youtube-icon.svg"
 import menu_icon from "../assets/menu.svg"
-import { useRef } from "react"
+import { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 function MainLogo () {
+    
+    const navigate = useNavigate() ;
+    const [isActive, setIsActive] = useState(false) ; 
+    const setActive = () => {setIsActive(!isActive)} ; 
 
-    const return_home = () => {
-        // Navigate back to home page when clicked 
+
+    const return_home = () => { 
+        navigate("/") ;  
     }
 
     const trigger_menu = () => {
-        console.log("hi") ; 
+        if (!isActive) {
+            document.getElementsByClassName("sidebar")[0].style.display = "none" ; 
+            document.getElementsByClassName("mini-sidebar")[0].style.display = "block" ;
+            setActive() ; 
+        }
+        else {
+            document.getElementsByClassName("sidebar")[0].style.display = "block" ; 
+            document.getElementsByClassName("mini-sidebar")[0].style.display = "none" ;
+            setActive() ; 
+        }
+        console.log(isActive) ; 
     }
     
     return ( 
-        <div className="main-logo" onClick={return_home}>
+        <div className="main-logo">
             <img className="wrap-icon" onClick={trigger_menu} id="menu-icon" src={menu_icon} alt="menu-icon"/>
-            <img id="youtube-icon" src={youtube_icon} alt="YouTube Icon"/> 
-            <div id="youtube-text">Fake-YouTube</div>
+            <img id="youtube-icon" src={youtube_icon} onClick={return_home} alt="YouTube Icon"/> 
+            <div id="youtube-text" onClick={return_home} >Fake-YouTube</div>
         </div> 
     )
 }
