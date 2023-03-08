e9d4316bf5d2f26fbadf4634005db8c4761962bc
Hankus Pankus <henryjacobscode@gmail.com>
2023-03-07 21:30:44 -0500

    Add personalized subscriptions on sidebar

--- Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/components/sidebar/signed-in-sidebar.js
+++ Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/components/sidebar/signed-in-sidebar.js
@@ -6,11 +6,59 @@ import { GeneralNavSection } from "./sidebar-comps/general-nav-section"
 import { AboutCategory } from "./sidebar-comps/miscallenous-section"
 import { ExploreCategories} from "./sidebar-comps/explore-categories-section" 
 import { ProfileRelatedLinks } from "./sidebar-comps/profile-related-links-section"
+import { useState, useContext, useEffect } from "react"
+import { userContext } from "../utils/contexts"
+import { getDocs, collection, query, where, doc, getDoc } from "@firebase/firestore" 
+import { firestore, storage } from "../../firebase/firebase"
+import { getDownloadURL, ref } from "firebase/storage"
 const temporararily_hold_7_items = new Array(7).fill(true); 
- 
+
+
 
 
 function SideBar () {
+
+
+    const [subscribedChannels, setSubscribedChannels] = useState(null) ;
+    const [subscribedProfilePhotos, setSubscribedProfileChannels] = useState(null) ;  
+    const {user, setUser} = useContext(userContext)
+
+    const get_subscribers = async () => {
+        const docRef = doc(firestore, "users", user.uid) ; 
+        const user_data = await getDoc(docRef) ; 
+        const subscribers = user_data.data().playlists.subscriptions  ; 
+       
+
+        const profile_photos_links = await Promise.all( 
+            subscribers.map( async (sub) => {
+              const subRef = doc(firestore, "users", sub) ; 
+              const getData = await getDoc(subRef) ; 
+              const link = getData.data().avatar ; 
+              const urlRef = ref(storage, link) ; 
+              const url = await getDownloadURL(urlRef) ; 
+              return url ; 
+            })
+         )
+
+         // Only show 7 subscriptions
+         if (profile_photos_links.length > 7){
+            profile_photos_links.splice(7, profile_photos_links.length) ; 
+        }
+
+        const temp_object = {}
+        profile_photos_links.map( (link, index) => {
+            temp_object[subscribers[index]] = link ; 
+        }) 
+        setSubscribedChannels(subscribers)
+        setSubscribedProfileChannels(temp_object) ; 
+        console.log(subscribers) ; 
+        console.log(temp_object) ;
+    }
+
+    useEffect( () => {
+        get_subscribers() ; 
+    }, [])
+
     return (
         <div className="sidebar">
             <div className="general-nav-links">
@@ -22,10 +70,12 @@ function SideBar () {
             <div className="subscriptions category-border" >
                 <h3 className="sidebar-subtitle">Subscriptions</h3>
                 {/* Replace this with user's subscriptions */}
-                { temporararily_hold_7_items.map( (item) => {
+                { subscribedChannels && setSubscribedProfileChannels && subscribedChannels.map( (sub, index) => {
+                    
                     return ( <Category 
-                        icon={default_profile}
-                        text="Cool Channel"
+                        icon={subscribedProfilePhotos[sub]}
+                        text={sub}
+                        key={sub} 
                     />)  
                 })}
                 <Category 
