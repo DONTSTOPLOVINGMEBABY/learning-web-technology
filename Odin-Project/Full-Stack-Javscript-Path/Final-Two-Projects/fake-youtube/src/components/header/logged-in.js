28a27379a7a05f29afbfaec6fd288ec1f4a90184
Hankus Pankus <henryjacobscode@gmail.com>
2023-03-19 16:56:11 -0400

    Make Upload Button Operational

--- Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/components/header/logged-in.js
+++ Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/components/header/logged-in.js
@@ -2,6 +2,7 @@ import upload_video from "../assets/upload-video.svg"
 import notifications_bell from "../assets/notifications-bell.svg"
 import default_profile_picture from "../assets/default-profile-picture.svg"
 import { useContext, useState, useRef, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import { userContext } from "../utils/contexts"
 import { profile_dropdown } from "../utils/export-image-objects"
 import Category from "../sidebar/sidebar-comps/category"
@@ -10,13 +11,17 @@ import Category from "../sidebar/sidebar-comps/category"
 function LoggedIn () {
 
     const {user, setUser} = useContext(userContext) ; 
+    const navigate = useNavigate() ; 
 
+    const upload_video_page = () => {
+        navigate("/upload-video") ; 
+    }
 
     return (
         <div className="logged-in">
             <div className="buttons">
                 <div className="wrap-icon">
-                    <img id="upload-video-icon" src={upload_video} alt="upload-video-icon"/>
+                    <img id="upload-video-icon" onClick={upload_video_page} src={upload_video} alt="upload-video-icon"/>
                 </div>
                 <div className="wrap-icon">
                     <img id="notifications-bell-icon" src={notifications_bell} alt="notifications-bell"/>
