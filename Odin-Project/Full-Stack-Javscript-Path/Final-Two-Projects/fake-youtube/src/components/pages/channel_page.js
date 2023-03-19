326fa5b7d88607ab7985fc79a695a6e161d8b512
Hankus Pankus <henryjacobscode@gmail.com>
2023-03-19 16:57:03 -0400

    Init Channel Page

---
+++ Odin-Project/Full-Stack-Javscript-Path/Final-Two-Projects/fake-youtube/src/components/pages/channel_page.js
@@ -0,0 +1,13 @@
+import {doc, getDoc, collection, getDocs, updateDoc, increment, arrayRemove, arrayUnion, query, where} from "@firebase/firestore"
+import { firestore } from "../../firebase/firebase"
+import "../../styles/channel-page.css" ; 
+
+function ChannelPage () {
+    return ( 
+        <div className="HomePage ChannelPage">
+    
+        </div>    
+    )
+}
+
+export default ChannelPage
