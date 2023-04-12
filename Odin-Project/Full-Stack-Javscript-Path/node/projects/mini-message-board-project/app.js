cf7b28dfd7444489a3f23589716c675165e930bc
Henry Jacobs <henryjacobscode@gmail.com>
2023-04-12 16:56:41 -0400

    End of the day push

--- Odin-Project/Full-Stack-Javscript-Path/node/projects/mini-message-board-project/app.js
+++ Odin-Project/Full-Stack-Javscript-Path/node/projects/mini-message-board-project/app.js
@@ -3,29 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const dotenv = require('dotenv')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-
-
 var app = express();
-dotenv.config()
-
-// Set up mongoose connection
-const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
-const mongoDB = process.env.connection_string
-
-main().catch(err => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-}
 
 // view engine setup
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
