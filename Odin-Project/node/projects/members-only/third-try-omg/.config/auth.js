2cb13e41f1968df60e03c0b4d9b9c9fc08597d5f
Hankus Pankus <henryjacobscode@gmail.com>
2023-04-25 15:23:38 -0400

    Write new auth middleware

--- Odin-Project/node/projects/members-only/third-try-omg/.config/auth.js
+++ Odin-Project/node/projects/members-only/third-try-omg/.config/auth.js
@@ -1,23 +1,55 @@
 const User = require('../models/users')
 
 module.exports = {
-    ensureAuthenticated: function(req, res, next) {
-      if (req.isAuthenticated()) {
-        return next();
-      }
-      res.redirect('/auth/login');
-    },
-    forwardAuthenticated: function(req, res, next) {
-      if (!req.isAuthenticated()) {
-        return next();
-      } 
-    }, 
-    forwardIsUser : async function (req, res, next) {
-      const user = await User.findById(req.user._conditions._id)
-      if (user.isMember){
-        return res.redirect("/")
-      }
-      next()
-    },
-};
+  hasAccount : function (req, res, next) {
+    if (!req.isAuthenticated()){
+      return res.redirect("/auth/login")
+    }
+    next()
+  },
+  checkNotAMember : async function (req, res, next) {
+    const thisUser = await User.findById(req.user._conditions._id)
+    if (!thisUser.isMember){
+      return res.redirect("/welcome")
+    }
+    next()
+  }, 
+  checkisAMember : async function (req, res, next) {
+    const thisUser = await User.findById(req.user._conditions._id)
+    if (thisUser.isMember){
+      return res.redirect("/")
+    }
+    next()
+  }, 
+  checkLoggedIn : function (req, res, next){ 
+    if (req.isAuthenticated()){
+      return res.redirect("/")
+    } 
+    next()
+  }
+}
+
+
+
+
+// module.exports = {
+//     ensureAuthenticated: function(req, res, next) {
+//       if (req.isAuthenticated()) {
+//         return next();
+//       }
+//       res.redirect('/auth/login');
+//     },
+//     forwardAuthenticated: function(req, res, next) {
+//       if (!req.isAuthenticated()) {
+//         return next();
+//       } 
+//     }, 
+//     forwardIsUser : async function (req, res, next) {
+//       const user = await User.findById(req.user._conditions._id)
+//       if (user.isMember){
+//         return res.redirect("/")
+//       }
+//       next()
+//     },
+// };
   
