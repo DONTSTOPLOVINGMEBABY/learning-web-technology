58ceaf0f1194de44d9e1bb4cb4c44b1aff066e0d
Hankus Pankus <henryjacobscode@gmail.com>
2023-05-03 21:24:16 -0400

    finish article preview

---
+++ Odin-Project/node/projects/blog-api/blog-admin-frontend/src/components/homepage/article-preview.jsx
@@ -0,0 +1,32 @@
+import styles from "../styles/article-preview.module.css"
+import authenticate_jwt from "../../utils/auth-jwt"
+import { useNavigate } from "react-router-dom"
+
+function ArticlePreview ({
+        author, 
+        categories, 
+        content, 
+        published, 
+        subtitle, 
+        title, 
+        uploadDate, 
+        id
+    }){
+
+    const navigate = useNavigate()
+
+    const load_article = async () => {
+        navigate(`/admin/articles/${id}`)
+    }
+
+    return (
+        <div onClick={load_article} className={styles.article_preview}>
+            <h1 className={styles.article_title}>{title}</h1>
+            <h3 className={styles.subtitle}>{subtitle}</h3>
+            <h3 className={styles.author}>By: {author}</h3>
+            <h3 className={styles.date}>Originally Uploaded: {`${new Date(uploadDate).getMonth() + 1}/${new Date(uploadDate).getDay()}/${new Date(uploadDate).getFullYear()}`}</h3>
+        </div>
+    )
+}
+
+export default ArticlePreview
