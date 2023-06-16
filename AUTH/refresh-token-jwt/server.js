57d7d1a6f7574ba872da403269a1d5b8b38e2f82
Hankus Pankus <henryjacobscode@gmail.com>
2023-06-16 13:00:59 -0400

    jwt with refresh-token example

---
+++ AUTH/refresh-token-jwt/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const everything_route = require('./routes/everything-4-now')
+dotenv.config()
+const morgan = require('morgan')
+
+
+// DB
+const db = process.env.CONNECTION_STRING
+async function connect_the_goose () {
+    let connection = await mongoose.connect(db)
+    console.log(`[CONNECTED] Connected to ${connection.connection.name}`)
+}
+connect_the_goose()
+
+
+// Middleware
+app.use(cors())
+app.use(express.urlencoded({ extended : true }))
+app.use(morgan('dev'))
+
+
+// Stuff 
+app.use("/", everything_route)
+
+
+app.listen(3000, () => {
+    console.log(`listening on port 3000`)
+})
+
+
